62534e094acba51bcd313ef1ed2f264368c5af60
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-07 19:05:06 +0200

    solve: add Lab-01-Т09, Т10 solution

---
+++ 01-DataTypesAndVariables-Lab/T09_DaysToMinutes/Program.cs
@@ -0,0 +1,5 @@
+﻿/*9*/
+var days = Console.ReadLine();
+var daysToInt =  int.Parse(days);
+var minutes = (daysToInt * 24) * 60;
+Console.WriteLine($"Minutes = {minutes}");
---
+++ 01-DataTypesAndVariables-Lab/T10_CalculateSpeed/Program.cs
@@ -0,0 +1,6 @@
+﻿/*10*/
+var speed = Console.ReadLine();
+var speedToFloat = float.Parse(speed);
+var distance = Console.ReadLine();
+var distanceToFloat = float.Parse(distance);
+Console.WriteLine($"{speedToFloat /distanceToFloat:f2}");
