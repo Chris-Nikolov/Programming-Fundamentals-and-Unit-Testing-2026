ba6a222112815d7504d29672e7a6aefa3f182b90
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-10 18:27:19 +0200

    solve: add Exercise-03-ТASK 01 solution

---
+++ 03-DataTypesVariablesKeyConcepts-Exercise/T01_ReversedNumbers/Program.cs
@@ -0,0 +1,7 @@
+﻿var firstNumber = double.Parse(Console.ReadLine());
+var secondNumber = double.Parse(Console.ReadLine());
+var thirdNumber = double.Parse(Console.ReadLine());
+
+Console.WriteLine(thirdNumber);
+Console.WriteLine(secondNumber);
+Console.WriteLine(firstNumber);
