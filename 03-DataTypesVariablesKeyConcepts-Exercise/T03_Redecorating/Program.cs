21b44e5b4e002376cdc582a7231a4c6c20c5b549
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-11 16:53:10 +0200

    solve: add Exercise-03-ТASK 03, T04 solution

---
+++ 03-DataTypesVariablesKeyConcepts-Exercise/T03_Redecorating/Program.cs
@@ -0,0 +1,30 @@
+﻿// Input
+
+var amountOfNylons = int.Parse(Console.ReadLine()) + 2;
+var amountOfPaint = int.Parse(Console.ReadLine()) * 1.1;
+var qtyOfThinner = int.Parse(Console.ReadLine());
+var hoursToFinish = int.Parse(Console.ReadLine());
+
+// prices in BGN
+
+var nylonPerSquareMeter = 1.50;
+var paintPerLiter = 14.50;
+var thinnerPerLiter = 5.00;
+var bags = 0.40;
+
+//Calculations
+
+var materialsCost = (amountOfNylons * nylonPerSquareMeter) 
+                    + (amountOfPaint * paintPerLiter) 
+                    + (qtyOfThinner * thinnerPerLiter) 
+                    + bags;
+
+var workPerHourCost = materialsCost * 0.30;
+
+var overallWorkCost = workPerHourCost * hoursToFinish;
+
+var totalCost = materialsCost + overallWorkCost;
+
+// Output
+
+Console.WriteLine(totalCost);
---
+++ 03-DataTypesVariablesKeyConcepts-Exercise/T04_FoodDelivery/Program.cs
@@ -0,0 +1,27 @@
+﻿// Input
+
+var qtyOfChickenMenus = int.Parse(Console.ReadLine());
+var qtyOfFishMenus = int.Parse(Console.ReadLine());
+var qtyOfVegetarianMenus = int.Parse(Console.ReadLine());
+
+// prices in BGN
+
+var priceOfChickenMenu = 10.35;
+var priceOfFishMenu = 12.40;
+var priceOfVegetarianMenu = 8.15;
+var priceOfDelivery = 2.50; 
+
+//Calculations
+
+var priceForMenus = (qtyOfChickenMenus * priceOfChickenMenu) 
+                    +  (qtyOfFishMenus * priceOfFishMenu)
+                    + (qtyOfVegetarianMenus * priceOfVegetarianMenu);
+
+var priceOfDessert = priceForMenus * 0.20;
+
+var totalOrderCost = priceForMenus + priceOfDessert + priceOfDelivery;
+
+
+// Output
+
+Console.WriteLine(totalOrderCost);
