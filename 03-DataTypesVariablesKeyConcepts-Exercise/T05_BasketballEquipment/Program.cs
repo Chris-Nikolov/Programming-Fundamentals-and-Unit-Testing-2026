9d79aac42f415a965be9002927b910f5eca57cbb
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-11 23:17:44 +0200

    solve: add Exercise-03-ТASK 05, T06 solution

---
+++ 03-DataTypesVariablesKeyConcepts-Exercise/T05_BasketballEquipment/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+
+var annualTrainingFee = int.Parse(Console.ReadLine());
+
+// prices
+
+var sneakersPrice = (annualTrainingFee * 0.60);
+var uniformPrice = sneakersPrice * 0.80;
+var ballPrice = uniformPrice * 0.25; 
+var accessoriesPrice = ballPrice * 0.20;
+
+var totalCost = annualTrainingFee + sneakersPrice + uniformPrice + ballPrice + accessoriesPrice;
+
+// Output
+
+Console.WriteLine(totalCost);
---
+++ 03-DataTypesVariablesKeyConcepts-Exercise/T06_Aquarium/Program.cs
@@ -0,0 +1,16 @@
+﻿// Input
+
+var lengthInCm = int.Parse(Console.ReadLine());
+var widthInCm = int.Parse(Console.ReadLine());
+var heightInCm = int.Parse(Console.ReadLine());
+var percentage = double.Parse(Console.ReadLine());
+
+//Calculations
+
+var volumeOfAquarium = lengthInCm * widthInCm * heightInCm;
+var volumeInLiters = volumeOfAquarium * 0.001;
+var requiredLiters = volumeInLiters * (1 - percentage / 100.00);
+
+// Output
+
+Console.WriteLine($"{requiredLiters:F2}");
