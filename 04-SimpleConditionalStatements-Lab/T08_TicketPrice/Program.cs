ddf38384d345f13121d303ff5b8d61791ada3438
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-14 18:41:01 +0200

    solve: add Lab-04-ТASK 08, Т09 solution

---
+++ 04-SimpleConditionalStatements-Lab/T08_TicketPrice/Program.cs
@@ -0,0 +1,10 @@
+﻿string ticketType = Console.ReadLine();
+
+string result = ticketType switch
+{
+    "student" => "1.00",
+    "regular" => "1.60",
+    _         => "Invalid ticket type!"
+};
+
+Console.WriteLine(result == "Invalid ticket type!" ? result : $"${result}");
---
+++ 04-SimpleConditionalStatements-Lab/T09_ValidTriangle/Program.cs
@@ -0,0 +1,7 @@
+﻿int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+bool isValid = (a + b > c) && (a + c > b) && (b + c > a);
+
+Console.WriteLine(isValid ? "Valid Triangle" : "Invalid Triangle");
