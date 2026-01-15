1ddebd86eec9f6ed3007ce244b95c104a86e5ea5
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-15 20:34:43 +0200

    solve: add Lab-05-ТASK 08, Т09 solution

---
+++ 05-ComplexConditionalStatements-Lab/T08_SortedNumbers/Program.cs
@@ -0,0 +1,16 @@
+﻿var firstNumber = int.Parse(Console.ReadLine());
+var secondNumber = int.Parse(Console.ReadLine());
+var thirdNumber = int.Parse(Console.ReadLine());
+
+if (firstNumber > secondNumber && secondNumber > thirdNumber) 
+{
+    Console.WriteLine("Descending");
+}
+else  if (firstNumber < secondNumber && secondNumber < thirdNumber)
+{
+    Console.WriteLine("Ascending");
+}
+else
+{
+    Console.WriteLine("Not sorted");
+}
---
+++ 05-ComplexConditionalStatements-Lab/T09_Cinema/Program.cs
@@ -0,0 +1,16 @@
+﻿var typeOfMovie = Console.ReadLine();
+var countOfRows = int.Parse(Console.ReadLine());
+var countOfSeats = int.Parse(Console.ReadLine());
+
+var pricesPerMovieType = new Dictionary<string, double>
+{
+    { "Premiere", 12.00 },
+    { "Normal", 7.50 },
+    { "Discount", 5.00 },
+};
+
+var totalSeats = countOfRows * countOfSeats;
+
+var totalPrice = pricesPerMovieType[typeOfMovie] * totalSeats;
+
+Console.WriteLine($"{totalPrice:F2}");
