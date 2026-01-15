18b19ab55ace6784e5645067bef48454d8a7f4af
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-15 17:02:13 +0200

    solve: add Lab-05-ТASK 02, Т03 solution

---
+++ 05-ComplexConditionalStatements-Lab/T02_LargestNumberOutOfThree/Program.cs
@@ -0,0 +1,5 @@
+﻿var firstNumber = int.Parse(Console.ReadLine());
+var secondNumber = int.Parse(Console.ReadLine());
+var thirdNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{Math.Max(firstNumber, Math.Max(secondNumber, thirdNumber))}");
