16db990399f23823520d2688cdf578999201de57
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-15 20:11:52 +0200

    solve: add Lab-05-ТASK 04, T05, T06, T07 solution

---
+++ 05-ComplexConditionalStatements-Lab/T04_FoodOrDrink/Program.cs
@@ -0,0 +1,23 @@
+﻿var product = Console.ReadLine();
+
+var foodProducts = new HashSet<string> 
+{
+    "curry", 
+    "noodles", 
+    "sushi", 
+    "spaghetti",
+    "bread"
+};
+
+var drinkProducts = new HashSet<string>
+{
+    "tea",
+    "water",
+    "coffee",
+    "juice" 
+};
+
+Console.WriteLine(foodProducts.Contains(product) 
+    ? "food" 
+    : (drinkProducts.Contains(product) ? "drink" : "unknown"));
+    
---
+++ 05-ComplexConditionalStatements-Lab/T07_VowelOrConstant/Program.cs
@@ -0,0 +1,12 @@
+﻿var letter = char.Parse(Console.ReadLine().ToLower());
+
+var vowels = new HashSet<char> 
+{
+    'a',
+    'e',
+    'i',
+    'o',
+    'u',
+};
+
+Console.WriteLine($"{(vowels.Contains(letter) ? "Vowel" : "Consonant")}");
