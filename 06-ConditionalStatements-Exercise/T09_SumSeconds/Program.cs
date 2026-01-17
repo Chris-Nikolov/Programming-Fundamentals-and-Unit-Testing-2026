f91adc2a74620b6c03bda4ec30e2bf4104eb0576
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-17 20:21:54 +0200

    solve: add Exercise-06-ТASK Т09, T10 solution

---
+++ 06-ConditionalStatements-Exercise/T09_SumSeconds/Program.cs
@@ -0,0 +1,10 @@
+﻿var time1 = int.Parse(Console.ReadLine());
+var time2 = int.Parse(Console.ReadLine());
+var time3 = int.Parse(Console.ReadLine());
+
+var totalTime = time1 + time2 + time3;
+
+var minutes = totalTime / 60;
+var seconds = totalTime % 60;
+
+Console.WriteLine($"{minutes}:{seconds:D2}");
---
+++ 06-ConditionalStatements-Exercise/T10_SummerOutfit/Program.cs
@@ -0,0 +1,49 @@
+﻿var temperature = int.Parse(Console.ReadLine());
+var timeOfDay = (Console.ReadLine());
+
+var outfit = "";
+var shoes = "";
+
+if (timeOfDay == "Morning") 
+{
+    switch (temperature)
+    {
+        case >= 10 and <= 18:
+            outfit = "Sweatshirt";
+            shoes = "Sneakers";
+            break;
+        case >= 18 and <= 24:
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+        case >= 25:
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+    }
+}
+else if (timeOfDay == "Afternoon")
+{
+    switch (temperature)
+    {
+        case >= 10 and <= 18:
+            outfit = "Shirt";
+            shoes = "Moccasins";
+            break;
+        case >= 18 and <= 24:
+            outfit = "T-Shirt";
+            shoes = "Sandals";
+            break;
+        case >= 25:
+            outfit = "Swim Suit";
+            shoes = "Barefoot";
+            break;
+    }
+}
+else if (timeOfDay == "Evening")
+{
+    outfit = "Shirt";
+    shoes = "Moccasins";
+}
+
+Console.WriteLine($"It's {temperature} degrees, get your {outfit} and {shoes}.");
