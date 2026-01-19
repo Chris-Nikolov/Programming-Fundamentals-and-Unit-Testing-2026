f2bdc940aa1f2d6235e367bfeaf1df980f865d72
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-19 17:38:04 +0200

    solve: add Lab-07-ТASK Т01 solution

---
+++ 07-ForLoop-Lab/T01_NumbersInRange/Program.cs
@@ -0,0 +1,7 @@
+﻿var firstNumber = int.Parse(Console.ReadLine());
+var secondNumber = int.Parse(Console.ReadLine());
+
+for (int i = firstNumber; i <= secondNumber; i++)
+{
+    Console.WriteLine(i);
+}
