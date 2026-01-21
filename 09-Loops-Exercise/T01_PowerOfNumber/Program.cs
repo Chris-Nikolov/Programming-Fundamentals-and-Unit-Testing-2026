cad651fb9960badf4e7c3ca7aa9faf9057ca9f53
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-21 20:25:42 +0200

    solve: add Exercise-09-ТASK 01 solution

---
+++ 09-Loops-Exercise/T01_PowerOfNumber/Program.cs
@@ -0,0 +1,11 @@
+﻿var number = int.Parse(Console.ReadLine());
+var power = int.Parse(Console.ReadLine());
+
+var result = 1;
+
+for (int i = 0; i < power; i++)
+{
+    result = result * number;
+}
+
+Console.WriteLine(result);
