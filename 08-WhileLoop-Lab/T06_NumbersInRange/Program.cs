0a3ce1dd1bcf22c2240703fe5d947ac2a4b1501a
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-21 20:14:45 +0200

    solve: add Lab-08-ТASK 05, T06, T07 solution

---
+++ 08-WhileLoop-Lab/T06_NumbersInRange/Program.cs
@@ -0,0 +1,13 @@
+﻿while (true)
+{
+    var number = int.Parse(Console.ReadLine());
+
+    if (number < 1 || number > 100)
+    {
+        continue;
+    }
+    
+    Console.WriteLine($"{number}");
+    break;
+    
+}
---
+++ 08-WhileLoop-Lab/T07_NumberProcessor/Program.cs
@@ -0,0 +1,19 @@
+﻿var number = int.Parse(Console.ReadLine());
+
+var command = Console.ReadLine();
+
+while (command != "End")
+{
+    if (command == "Inc")
+    {
+        number++;
+    }
+    else if (command == "Dec")
+    {
+        number--;
+    }
+    
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(number);
