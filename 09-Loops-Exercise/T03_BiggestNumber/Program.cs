28f0021102bbdb68e40256b28476e3bc187d35aa
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-22 17:08:05 +0200

    solve: add Exercise-09-ТASK 02, T03, T04 solution

---
+++ 09-Loops-Exercise/T03_BiggestNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+var biggestNumber = int.MinValue;
+
+for (var i = 1 ; i <= n; i++)
+{
+    var currNumber = int.Parse(Console.ReadLine());
+    
+    if (currNumber > biggestNumber)
+    {
+        biggestNumber = currNumber;
+    }
+}
+
+Console.WriteLine(biggestNumber);
---
+++ 09-Loops-Exercise/T04_VowelSum/Program.cs
@@ -0,0 +1,27 @@
+﻿var n =  int.Parse(Console.ReadLine());
+
+var vowels = new Dictionary<char, int>
+{
+    {'a', 1},
+    {'e', 2},
+    {'i', 3},
+    {'o', 4},
+    {'u', 5},
+
+};
+
+var vowelSum = 0;
+
+for (var i = 0; i < n; i++)
+{
+    var letter = char.Parse(Console.ReadLine());
+
+    if (vowels.ContainsKey(letter))
+    {
+        vowelSum += vowels[letter];
+    }
+    
+    
+}
+
+Console.WriteLine(vowelSum);
