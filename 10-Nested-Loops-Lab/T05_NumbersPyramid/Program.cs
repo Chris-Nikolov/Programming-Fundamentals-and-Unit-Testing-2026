c5e8a87ff173267cc065745fe24931dc0cabc461
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-29 19:26:58 +0200

    solve: add Lab-10-ТASK 05 solution

---
+++ 10-Nested-Loops-Lab/T05_NumbersPyramid/Program.cs
@@ -0,0 +1,17 @@
+﻿var n = int.Parse(Console.ReadLine());
+var currentNumber = 1;
+var elementsOnRow = 1;
+
+while (currentNumber <= n)
+{
+    for (var j = 0; j < elementsOnRow; j++)
+    {
+        if (currentNumber <= n)
+        {
+            Console.Write($"{currentNumber} ");
+            currentNumber++;
+        }
+    }
+    Console.WriteLine();
+    elementsOnRow++;
+}
