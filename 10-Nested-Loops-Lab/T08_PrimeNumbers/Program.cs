f838c028307dfeff122abb5ac2d1bfd321b11d3f
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-30 22:06:23 +0200

    solve: add Lab-10-ТASK 08 solution

---
+++ 10-Nested-Loops-Lab/T08_PrimeNumbers/Program.cs
@@ -0,0 +1,23 @@
+﻿var start = int.Parse(Console.ReadLine());
+var stop = int.Parse(Console.ReadLine());
+
+for (var num = start; num <= stop; num++)
+{
+    if (num < 2) continue;
+
+    var isPrime = true;
+    
+    for (var i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            isPrime = false;
+            break;
+        }
+    }
+    
+    if (isPrime)
+    {
+        Console.Write(num + " ");
+    }
+}
