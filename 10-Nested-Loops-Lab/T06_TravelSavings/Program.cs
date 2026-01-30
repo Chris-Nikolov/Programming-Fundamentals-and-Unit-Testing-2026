ce8770d167825ab51f3064a62cce5b6f6a7fe4c0
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-01-30 21:51:19 +0200

    solve: add Lab-10-ТASK 06, T07 solution

---
+++ 10-Nested-Loops-Lab/T06_TravelSavings/Program.cs
@@ -0,0 +1,40 @@
+﻿double collectedMoney = 0;
+var stopper = false;
+
+while (true)
+{
+    var destination = Console.ReadLine();
+    
+    if (destination == "End")
+    {
+        stopper = true;
+    }
+    
+    if (stopper)
+    {
+        break;
+    }
+    
+    var price = double.Parse(Console.ReadLine());
+
+    while (true)
+    {
+        var command = Console.ReadLine();
+        
+        if (command == "End")
+        {
+            stopper = true;
+            break;
+        }
+        
+        var currMoney = double.Parse(command);
+        collectedMoney += currMoney;
+        Console.WriteLine($"Collected: {collectedMoney:F2}");
+        if (collectedMoney >= price)
+        {
+            Console.WriteLine($"Going to {destination}!");
+            collectedMoney = 0;
+            break;
+        }
+    }
+}
---
+++ 10-Nested-Loops-Lab/T07_SumOfDigitsCalculator/Program.cs
@@ -0,0 +1,17 @@
+﻿var input = Console.ReadLine();
+
+while (input != "End")
+{
+    
+    var sumOfDigits = 0;
+    
+    for (var i = 0; i < input.Length; i++)
+    {
+        sumOfDigits += int.Parse(input[i].ToString());
+    }
+    Console.WriteLine($"Sum of digits = {sumOfDigits}");
+    
+    input = Console.ReadLine();
+}
+
+Console.WriteLine("Goodbye");
