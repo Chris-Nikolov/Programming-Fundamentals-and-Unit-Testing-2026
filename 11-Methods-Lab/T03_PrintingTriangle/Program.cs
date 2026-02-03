d3e2c6919a4b308051a2d8d7dda365596e387db7
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-03 20:44:44 +0200

    solve: add Lab-11-ТASK 02, T03 solution

---
+++ 11-Methods-Lab/T03_PrintingTriangle/Program.cs
@@ -0,0 +1,27 @@
+﻿var n = int.Parse(Console.ReadLine());
+
+void PrintTriangle(int number)
+{
+    for (var row = 1; row <= number; row++)
+    {
+        for (var col = 1; col <= row; col++)
+        {
+            Console.Write(col + " ");
+        }
+
+        Console.WriteLine();
+    }
+
+    for (var row = number - 1; row >= 1; row--)
+    {
+
+        for (var col = 1; col <= row; col++)
+        {
+            Console.Write(col + " ");
+        }
+
+        Console.WriteLine();
+    }
+}
+
+PrintTriangle(n);
