1be3d001dccdebd295939c436c4ed27104ab2cd4
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-06 01:07:44 +0200

    solve: add Lab-11-ТASK 06, T07 solution

---
+++ 11-Methods-Lab/T06_MathPower/Program.cs
@@ -0,0 +1,9 @@
+﻿var baseNumber = int.Parse(Console.ReadLine());
+var powerNumber = int.Parse(Console.ReadLine());
+
+static  int MathPower(int a, int b)
+{
+    return (int)Math.Pow(a, b);
+}
+
+Console.WriteLine(MathPower(baseNumber, powerNumber));
---
+++ 11-Methods-Lab/T07_GreaterOfTwoValues/Program.cs
@@ -0,0 +1,35 @@
+﻿var currType = Console.ReadLine();
+
+void GetMax(string type)
+{
+    var input1 = Console.ReadLine();
+    var input2 = Console.ReadLine();
+
+    switch (type)
+    {
+        case "int":
+            var a = int.Parse(input1);
+            var b = int.Parse(input2);
+            Console.WriteLine(Math.Max(a, b));
+            break;
+
+        case "char":
+            var c1 = char.Parse(input1);
+            var c2 = char.Parse(input2);
+            Console.WriteLine((char)Math.Max(c1, c2));
+            break;
+
+        case "string":
+            if (input1.CompareTo(input2) >= 0)
+            {
+                Console.WriteLine(input1);
+            }
+            else
+            {
+                Console.WriteLine(input2);
+            }
+            break;
+    }
+}
+
+GetMax(currType);
