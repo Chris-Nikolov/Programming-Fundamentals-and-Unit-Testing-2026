62563a04223334edccddee5c5e137510de8b816d
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-05 19:36:58 +0200

    solve: add Lab-11-ТASK 04, T05 solution

---
+++ 11-Methods-Lab/T04_CalculateRectangleArea/Program.cs
@@ -0,0 +1,9 @@
+﻿var width = int.Parse(Console.ReadLine());
+var length = int.Parse(Console.ReadLine());
+
+static int RectangleArea(int x, int y)
+{
+    return x * y;
+}
+
+Console.WriteLine(RectangleArea(width, length));
---
+++ 11-Methods-Lab/Т05_RepeatString/Program.cs
@@ -0,0 +1,16 @@
+﻿var text = Console.ReadLine();
+var number = int.Parse(Console.ReadLine());
+
+
+static string RepeatString(string str, int count)
+{
+    var result = "";
+    
+    for (var i = 0; i < count; i++)
+    {
+        result += str;
+    }
+    return result;
+}
+
+Console.WriteLine(RepeatString(text, number));
