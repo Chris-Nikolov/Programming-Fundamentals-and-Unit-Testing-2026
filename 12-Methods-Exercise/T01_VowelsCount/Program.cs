02e2afc0419af5e436ae17ff2983d83e1236213a
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-07 22:15:12 +0200

    solve: add Exercise-12-ТASK 01 solution

---
+++ 12-Methods-Exercise/T01_VowelsCount/Program.cs
@@ -0,0 +1,20 @@
+﻿var text = Console.ReadLine().ToLower();
+
+static void VowelsCount(string word)
+{
+    var vowels = new char [5] { 'a', 'e', 'i', 'o', 'u' };
+
+    int result = 0;
+
+    foreach (var ch in word)
+    {
+        if (vowels.Contains(ch))
+        {
+            result += 1;
+        }
+    }
+    
+    Console.WriteLine(result);
+}
+
+VowelsCount(text);
