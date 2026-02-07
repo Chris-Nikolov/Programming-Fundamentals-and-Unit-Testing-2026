afd85754c4727c8c2be3798bf37ad7161ce80dd7
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-07 22:43:38 +0200

    solve: add Exercise-12-ТASK 02, T03 solution

---
+++ 12-Methods-Exercise/T02_FactorialDivision/Program.cs
@@ -0,0 +1,19 @@
+﻿var number1 = int.Parse(Console.ReadLine());
+var number2 = int.Parse(Console.ReadLine());
+
+static int CalculateFactorial(int number)
+{
+    var factorial = 1;
+    for (var i = 1; i <= number; i++)
+    {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+var firstFactorial = CalculateFactorial(number1);
+var secondFactorial = CalculateFactorial(number2);
+
+var result = firstFactorial / secondFactorial;
+
+Console.WriteLine(result);
---
+++ 12-Methods-Exercise/T03_MultiplicationSign/Program.cs
@@ -0,0 +1,30 @@
+﻿var num1 = int.Parse(Console.ReadLine());
+var num2 = int.Parse(Console.ReadLine());
+var num3 = int.Parse(Console.ReadLine());
+
+static void MultiplicationSign(int x, int y, int z)
+{
+    if (x == 0 || y == 0 || z == 0)
+    {
+        Console.WriteLine("zero");
+    }
+    else
+    {
+        var negativeCount = 0;
+
+        if (x < 0) negativeCount++;
+        if (y < 0) negativeCount++;
+        if (z < 0) negativeCount++;
+
+        if (negativeCount % 2 != 0)
+        {
+            Console.WriteLine("negative");
+        }
+        else
+        {
+            Console.WriteLine("positive");
+        }
+    }
+}
+
+MultiplicationSign(num1, num2, num3);
