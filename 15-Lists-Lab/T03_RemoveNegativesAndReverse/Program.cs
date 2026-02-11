3b27d167d3eaef11be23756c759c8d6742e01767
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-11 22:43:15 +0200

    solve: add Lab-15-ТASK 03 solution

---
+++ 15-Lists-Lab/T03_RemoveNegativesAndReverse/Program.cs
@@ -0,0 +1,16 @@
+﻿var listOfNumbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+listOfNumbers.RemoveAll(x => x < 0);
+listOfNumbers.Reverse();
+
+if (listOfNumbers.Count == 0)
+{
+    Console.WriteLine("empty");
+}
+else
+{
+    Console.WriteLine($"{string.Join(" ", listOfNumbers)}");
+}
