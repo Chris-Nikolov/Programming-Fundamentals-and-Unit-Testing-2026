f76a6015e6bd94904f803869d083abbce0558ad6
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-11 16:34:18 +0200

    solve: add Lab-13-ТASK 04 solution

---
+++ 13-Arrays-Lab/T04_EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,21 @@
+﻿var arrayOfNumbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var oddNumbers = 0;
+var evenNumbers = 0;
+
+foreach (var number in arrayOfNumbers) 
+{
+    if (number % 2 == 0) 
+    {
+        evenNumbers += number;
+    } 
+    else
+    {
+        oddNumbers += number;
+    }
+}
+
+Console.WriteLine(evenNumbers - oddNumbers);
