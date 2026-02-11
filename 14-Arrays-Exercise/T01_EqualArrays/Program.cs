9388afae1a9393158fa0503aa819d79f306c33c6
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-11 17:41:30 +0200

    solve: add Exercise-14-ТASK 01, T02 solution

---
+++ 14-Arrays-Exercise/T01_EqualArrays/Program.cs
@@ -0,0 +1,31 @@
+﻿var firstArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var secondArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var areIdentical = true;
+
+
+for (var i = 0; i < firstArray.Length; i++)
+{
+    if (firstArray[i] != secondArray[i])
+    {
+            areIdentical = false;
+            break;
+    }
+}
+
+
+if (areIdentical)
+{
+    Console.WriteLine("Arrays are identical.");
+}
+else
+{
+    Console.WriteLine("Arrays are not identical.");
+}
---
+++ 14-Arrays-Exercise/T02_CommonElements/Program.cs
@@ -0,0 +1,22 @@
+﻿var firstArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var secondArray = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var commonElements = new List<int>();
+
+for (var i = 0; i < firstArray.Length; i++)
+{
+    if (secondArray.Contains(firstArray[i]))
+    {
+        commonElements.Add(firstArray[i]);
+        
+    }
+}
+
+Console.WriteLine(string.Join(" ", commonElements));
