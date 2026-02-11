92c725fc52dac796c747119328d6f35fbd3a97ce
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-11 16:00:38 +0200

    solve: add Lab-13-ТASK 01 solution

---
+++ 13-Arrays-Lab/T01_DayOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿string[] week = new string[7]
+{
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday"
+};
+
+var number =  int.Parse(Console.ReadLine());
+
+if (number <= week.Length && number > 0)
+{
+    Console.WriteLine(week[number - 1]);
+}
+else
+{
+    Console.WriteLine("Invalid day!");
+}
