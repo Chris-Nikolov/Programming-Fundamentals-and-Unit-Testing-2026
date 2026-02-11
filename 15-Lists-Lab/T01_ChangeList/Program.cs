b1c7029ca5585a384e92a76d2c0ca5286754f52d
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-11 22:37:48 +0200

    solve: add Lab-15-ТASK 01, T02 solution

---
+++ 15-Lists-Lab/T01_ChangeList/Program.cs
@@ -0,0 +1,32 @@
+﻿var arrOfNumbers = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+var command = Console.ReadLine();
+
+while (command != "end")
+{
+    var commandArgs = command.Split().ToList();
+    var commandName = commandArgs[0];
+    
+    if (commandName == "Delete")
+    {
+        arrOfNumbers.RemoveAll(x => x == int.Parse(commandArgs[1]));
+    }
+    
+    else if (commandName == "Insert")
+    {
+        var value = int.Parse(commandArgs[1]);
+        var index = int.Parse(commandArgs[2]);
+        if (index >= 0 && index <= arrOfNumbers.Count)
+        {
+            arrOfNumbers.Insert(index, value);
+        }
+        
+    }
+    
+    command = Console.ReadLine();
+}
+
+Console.WriteLine(string.Join(" ", arrOfNumbers));
---
+++ 15-Lists-Lab/T02_ListOfProducts/Program.cs
@@ -0,0 +1,15 @@
+﻿var n =  int.Parse(Console.ReadLine());
+var products = new List<string>();
+
+for (var i = 0; i < n; i++)
+{
+    var product = Console.ReadLine();
+    products.Add(product);
+}
+
+products.Sort();
+
+for (var i = 1; i <= products.Count(); i++)
+{
+    Console.WriteLine($"{i}.{products[i-1]}");
+}
