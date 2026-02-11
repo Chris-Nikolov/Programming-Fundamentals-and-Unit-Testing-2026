5ce972273fa8281234716f7c1efcbae94d5fc42b
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-11 18:03:53 +0200

    solve: add Exercise-14-ТASK 03, T04 solution

---
+++ 14-Arrays-Exercise/T03_CondenseArrayToNumber/Program.cs
@@ -0,0 +1,18 @@
+﻿var nums = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+while (nums.Length > 1)
+{
+    var condensed = new int[nums.Length - 1];
+
+    for (var i = 0; i < condensed.Length; i++)
+    {
+        condensed[i] = nums[i] + nums[i + 1];
+    }
+    
+    nums = condensed;
+}
+
+Console.WriteLine(nums[0]);
---
+++ 14-Arrays-Exercise/T04_MagicSum/Program.cs
@@ -0,0 +1,21 @@
+﻿var nums = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToArray();
+
+var controlNum = int.Parse(Console.ReadLine());
+
+
+for (var i = 0; i < nums.Length; i++)
+{
+    for (var j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] + nums[j] == controlNum)
+        {
+            if (nums[i] + nums[j] == controlNum)
+            {
+                Console.WriteLine($"{nums[i]} {nums[j]}");
+            }
+        }
+    }
+}
