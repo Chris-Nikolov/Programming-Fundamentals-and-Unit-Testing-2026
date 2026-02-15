88ce5f14e20d51a2c60016c18c9db2cb803999fa
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-15 23:09:16 +0200

    solve: add Exercise-16-ТASK 02 solution

---
+++ 16-Lists-Exercise/T02_HouseParty/Program.cs
@@ -0,0 +1,27 @@
+﻿var n =  int.Parse(Console.ReadLine());
+
+var partyList = new List<string>();
+
+for (var i =  0; i < n; i++)
+{
+    var input = Console.ReadLine().Split();
+    
+    if (partyList.Contains(input[0]) && input[2] == "not")
+    {
+        partyList.Remove(input[0]);
+    }
+    else if (!(partyList.Contains(input[0])) && input[2] != "not")
+    {
+        partyList.Add(input[0]);
+    }
+    else if (!(partyList.Contains(input[1])) && input[2] == "not")
+    {
+        Console.WriteLine($"{input[0]} is not in the list!");
+    }
+    else
+    {
+        Console.WriteLine($"{input[0]} is already in the list!");
+    }
+}
+
+Console.WriteLine(string.Join("\n", partyList));
