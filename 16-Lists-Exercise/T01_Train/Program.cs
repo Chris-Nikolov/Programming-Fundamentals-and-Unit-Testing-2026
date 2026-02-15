03d5bfae7db42fdfdc1cc62cd5d555bb806dc2f1
Chris Nikolov <145293090+Chris-Nikolov@users.noreply.github.com>
2026-02-15 22:45:38 +0200

    solve: add Exercise-16-ТASK 01 solution

---
+++ 16-Lists-Exercise/T01_Train/Program.cs
@@ -0,0 +1,39 @@
+﻿var wagons = Console.ReadLine()
+    .Split()
+    .Select(int.Parse)
+    .ToList();
+
+var maxCapacityPerWagon = int.Parse(Console.ReadLine());
+
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == "end")
+    {
+        break;
+    }
+    
+    var command = input.Split();
+    var commandName = command[0];
+    
+    if (commandName == "Add")
+    {
+        var commandValue = int.Parse(command[1]);
+        
+        wagons.Add(commandValue);
+        continue;
+    }
+    
+    var value = int.Parse(command[0]);
+    
+    for (var i =  0; i < wagons.Count; i++)
+    {
+        if (wagons[i] + value <= maxCapacityPerWagon)
+        {
+            wagons[i] += value;
+            break;
+        }
+    }
+}
+
+Console.WriteLine(string.Join(" ", wagons));
